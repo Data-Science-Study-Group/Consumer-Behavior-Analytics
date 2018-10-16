81d77bba5089aa93765f41add9c90111d8fe1194
troykirin <troykirin@gmail.com>
2018-10-16 16:07:37 -0700

    Beginning of data cleaning, imported datasets from project GitHub.

---
+++ data_cleaning.R
@@ -0,0 +1,23 @@
+#using dplyr for data manipulation
+library("dplyr")
+
+#importing data from project github for universal access
+df_geo_loc = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/geolocation_olist_public_dataset.csv")
+df_olist_classified = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/olist_classified_public_dataset.csv")
+df_olist_v2 = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/olist_public_dataset_v2.csv")
+df_olist_v2_customers = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/olist_public_dataset_v2_customers.csv")
+df_olist_v2_payments = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/olist_public_dataset_v2_payments.csv")
+df_product_category_name_translation = read.csv("https://raw.githubusercontent.com/troykirin/datasci_proj/master/brazilian-ecommerce/product_category_name_translation.csv")
+
+names(df_geo_loc)
+
+#these two df seem to be correlated although utilizes different column names, will have to clean
+names(df_olist_classified)
+names(df_olist_v2)
+
+names(df_olist_v2_customers)
+names(df_olist_v2_payments)
+names(df_product_category_name_translation)
+
+sum(is.na(df_geo_loc))
+sum(is.na(df))
