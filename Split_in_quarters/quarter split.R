782b16db49a89f7cfada5ca5e6ff28ee27317d79
troykirin <troykirin@gmail.com>
2018-12-03 23:56:46 -0800

    Work on creating using qplot instead of ggplot.

--- Split_in_quarters/quarter split.R
+++ Split_in_quarters/quarter split.R
@@ -42,12 +42,35 @@ subset(df, df$`df.time[, 1]` > "2016-10-01" & df$`df.time[, 1]` < "2016-12-31") 
 
 #2017
 subset(df, df$`df.time[, 1]`  > "2017-01-01" & df$`df.time[, 1]` < "2017-03-31") -> df_q1_2017
+df_q1_2017$group = "q1_2017"
 
 subset(df, df$`df.time[, 1]` > "2017-04-01" & df$`df.time[, 1]` < "2017-6-30") -> df_q2_2017
+df_q2_2017$group = "q2_2017"
 
 subset(df, df$`df.time[, 1]` > "2017-07-01" & df$`df.time[, 1]` < "2017-09-30") -> df_q3_2017
+df_q3_2017$group = "q3_2017"
 
 subset(df, df$`df.time[, 1]` > "2017-10-01" & df$`df.time[, 1]` < "2017-12-31") -> df_q4_2017
+df_q4_2017$group = "q4_2017"
+
+rbind(df_q1_2017,df_q2_2017) ->main_df
+rbind(main_df,df_q3_2017) -> main_df
+rbind(main_df,df_q4_2017) -> main_df
+
+View(main_df)
+
+main_df %>%
+  filter(main_df$product_category_name_english %in% "health_beauty") -> main_df_h_and_b
+
+ggplot(data = main_df,
+       aes(x = as.numeric(main_df$order_purchase_timestamp),
+           y = main_df$price)) +
+  geom_point() +
+  geom_smooth(method = 'glm', aes(color=group)) +
+  ggtitle("Regression of 2017 Health and Beauty") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(x="Purchased",y="Price")
+
 
 #2018
 subset(df, df$`df.time[, 1]`  > "2018-01-01" & df$`df.time[, 1]` < "2018-03-31") -> df_q1_2018
@@ -63,11 +86,11 @@ subset(df, df$`df.time[, 1]` > "2018-10-01" & df$`df.time[, 1]` < "2018-12-31") 
 df_q1_2017 %>%
   filter(df_q1_2017$product_category_name_english %in% "health_beauty") -> df_q1_2017_h_and_b
 
-ggplot(data = df_q1_2017_h_and_b,
+qplot(data=df_q1_2017_h_and_b,x=as.numeric(df_q1_2017_h_and_b$order_purchase_timestamp),y=df_q1_2017_h_and_b$price, geom = c("point","smooth"))
+
+qplot(data = df_q1_2017_h_and_b,
        aes(x = as.numeric(df_q1_2017_h_and_b$order_purchase_timestamp),
-           y = df_q1_2017_h_and_b$price)) +
-  geom_point(color='red') +
-  geom_smooth(method = 'glm') +
+           y = df_q1_2017_h_and_b$price), geom = c("point","smooth")) +
   ggtitle("Regression of Q1-2017 Health and Beauty") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(x="Purchased",y="Price")
