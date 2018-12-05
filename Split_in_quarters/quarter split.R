d3606a7b417f16267bd9c9dd51f265dfbab8ca2f
troykirin <troykirin@gmail.com>
2018-12-04 17:46:33 -0800

    Filter out top 5 categories.

--- Split_in_quarters/quarter split.R
+++ Split_in_quarters/quarter split.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(data.table)
 
 orders = read.csv("~/GitHub/datasci_proj/brazilian-ecommerce_ver6/olist_orders_dataset.csv",na.strings = c("","NA"))
 customers = read.csv("~/GitHub/datasci_proj/brazilian-ecommerce_ver6/olist_customers_dataset.csv")
@@ -12,7 +13,6 @@ df<- merge(df, customers, by="customer_id")
 
 merge(df, product_category, by="product_id") -> df
 merge(df, product_trans, by="product_category_name") -> df
-View(df)
 #
 
 #Split into quarters
@@ -28,8 +28,83 @@ df %>%
 df %>%
   mutate(df.time[,2]) -> df
 
+#create frequency table of num of items bought in a day
+table(df$`df.time[, 1]`) %>%
+  as.data.frame() -> new_new
+
+#filter top by cat
+table(df$product_category_name_english) %>%
+  as.data.frame() -> new_cat
+new_cat %>%
+  arrange(-Freq) -> new_cat
+new_cat = new_cat[1:5,]
+#
+#rename col
+names(new_cat) = c("product_category_name_english","Freq_Prod")
+#
+
+#merge back
+left_join(x=df,y=new_cat,by="product_category_name_english") -> df
+#
+
+#filter out na
+df %>%
+  filter(!is.na(Freq_Prod)) -> df
+#
+
+#filter out categories == 0
+df_temp <- subset(x = df, df$Freq_Prod != 0)
+
+
 #convert to date type
 df$`df.time[, 1]` <- as.Date(df$`df.time[, 1]`, format= "%Y-%m-%d")
+new_new$Day_Pur <- as.Date(new_new$Day_Pur, format = "%Y-%m-%d")
+
+#rename new_new
+#
+setnames(df, old=c("df.time[, 1]"),new=c("Day_Pur"))
+names(new_new) = c("Day_Pur","Freq")
+#
+View(new_new)
+View(df)
+#
+
+#keep only top 5
+new_new %>%
+  arrange(-Freq) -> new_new
+new_new[1:5,] -> new_new
+#
+
+##join
+left_join(x=df,y=new_new,by="Day_Pur") -> df
+View(df)
+##
+
+#remove rows with NA
+df %>% 
+  filter(!is.na(Freq)) -> df
+#
+
+#add quarter column
+
+##
+
+##filter only top 5 categories
+
+#
+
+
+#plotting new_new for a freq table instead of regression to price
+qplot(data=df_temp,x=as.numeric(Day_Pur),y=Freq,color=df_temp$product_category_name_english) + 
+  geom_point() +
+  geom_smooth(method = "glm",color="red")+
+  ggtitle("")+
+  labs(x="Purchased Date",y="Number of Products Bought")
+
+reg <- lm(data=new_new, Freq ~  as.numeric(Var1))
+summary(reg)
+plot(reg)
+##
 
 #sort out only 2016
 subset(df, df$`df.time[, 1]`  > "2016-01-01" & df$`df.time[, 1]` < "2016-03-31") -> df_q1_2016
@@ -83,40 +158,74 @@ main_df %>%
 
 #plot by category
 
+#test
+x = as.numeric(main_df_h_and_b$order_purchase_timestamp)
+y=log(main_df_h_and_b$price)
+
+plot(x,y)
+
+ggplot(data = main_df_h_and_b,
+       aes(x = as.numeric(order_purchase_timestamp),
+           y = price) +
+         geom_point(aes(color=group)) +
+         geom_smooth(method = 'glm', aes(color=group),fill="black") +
+         ggtitle("Regression of 2017 Health and Beauty") +
+         theme(plot.title = element_text(hjust = 0.5)) +
+         labs(x="Purchased",y="Price")+
+         coord_cartesian(ylim=c(0, 250))
+#test
+
 #health and beauty
 ggplot(data = main_df_h_and_b,
        aes(x = as.numeric(order_purchase_timestamp),
-           y = price)) +
+           y = price) +
   geom_point(aes(color=group)) +
   geom_smooth(method = 'glm', aes(color=group),fill="black") +
   ggtitle("Regression of 2017 Health and Beauty") +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(x="Purchased",y="Price")+
   coord_cartesian(ylim=c(0, 250))
+
+#reg
+reggresor <- lm(data = main_df_h_and_b, (price) ~ as.numeric(order_purchase_timestamp))
+summary(reggresor)
+plot(reggresor)
+##
+
 #
 
 #computer accessories
 ggplot(data = main_df_comp_ass,
        aes(x = as.numeric(order_purchase_timestamp),
-           y = price)) +
-  geom_point() +
-  geom_smooth(method = 'glm', aes(color=group)) +
+           y = log(price))) +
+  geom_point(aes(color=group)) +
+  geom_smooth(method = 'glm', aes(color=group),fill="black") +
   ggtitle("Regression of 2017 Computer Accessories") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  labs(x="Purchased",y="Price")+
-  coord_cartesian(ylim=c(0, 250))
+  labs(x="Purchased",y="Price")
+#reg
+reggresor <- lm(data = main_df_comp_ass, log(price) ~ as.numeric(order_purchase_timestamp))
+summary(reggresor)
+plot(reggresor)
+##
+
 ##
 
 #sports_leisure
 ggplot(data = main_df_sport_lei,
        aes(x = as.numeric(order_purchase_timestamp),
-           y = price)) +
-  geom_point() +
-  geom_smooth(method = 'glm', aes(color=group)) +
+           y = log(price))) +
+  geom_point(aes(color=group)) +
+  geom_smooth(method = 'glm', aes(color=group),fill="black") +
   ggtitle("Regression of 2017 Sports Leisure") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  labs(x="Purchased",y="Price")+
-  coord_cartesian(ylim=c(0, 250))
+  labs(x="Purchased",y="Price")
+#reg
+reggresor <- lm(data = main_df_sport_lei, log(price) ~ as.numeric(order_purchase_timestamp))
+summary(reggresor)
+plot(reggresor)
+##
+
 ##
 
 ##end plots
